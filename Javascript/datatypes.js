9792098b6d818555bd3bffbef71ce672abefea4e
purushottam123pro <purushottamsingh425@gmail.com>
2023-02-13 19:54:27 +0530

    commit

---
+++ Javascript/datatypes.js
@@ -0,0 +1,35 @@
+
+/*basic programming fundamental to learn code is how to save data and learn about variable*/
+
+// values and datatypes 
+
+// primitive datatype
+
+
+// - All numbers are treated same - 2,4,4.5,24
+// - Strings: 'pwskills' "purushottam"
+// - boolean: true or false 
+// - null means nothing, empty, cannot be used for 0 because 0 is temperature 
+// - undefined 
+
+let a; 
+// unndefined is not defined, can be defined in future 
+
+
+
+
+-// Non primitive data types
+// Array
+
+let name1= "hitesh"
+let name2= "purushottam"
+let name3= "Rohit"
+
+// use array instead of declaring different variable
+
+let name=["hitesh","purushottam","Rohit",2,4,true,]
+
+// Object {}  difficult to master 
+
+// object and browser event (most impoertant for javascript)
+
