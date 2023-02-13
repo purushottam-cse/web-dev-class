c9e0e30ed661b672b5272cb6eb636b7d7a796ba2
purushottam123pro <purushottamsingh425@gmail.com>
2023-02-13 20:44:21 +0530

    commit

---
+++ Javascript/firstp.js
@@ -0,0 +1,60 @@
+let highScore = 3000
+console.log(highScore)
+
+let pwSkillsCoursePrice = 3200
+console.log(pwSkillsCoursePrice)
+
+// Assignment operator 
+//  =
+
+
+let userName = "Purushottam"
+console.log(userName);
+
+const totalPrice = 200
+let gstOnCourse = 36
+let finalPurchaseAmount = totalPrice + gstOnCourse
+console.log("final amount to be paid:", finalPurchaseAmount);
+
+let pwDsaCourse=500
+console.log("final amount to be paid:",  pwDsaCourse+ gstOnCourse);
+
+console.log("Answer is:", 5%2 ) // modulus
+console.log("Answer is:", 5/2 )
+console.log("Answer is:", 5**2 ) // power to
+
+let value1=200
+let value2=300
+console.log(value1==value2); // camparison
+console.log(value1!=value2); // camparison
+console.log(value1>value2); // camparison
+console.log(value1<value2); // camparison
+
+let value3="300"
+
+console.log(value2==value3); // == convert then compare automatically if different types are compared
+
+console.log(value2===value3); // type comparison
+
+let value4=true
+let value5=false
+let value6=true
+let value7=false
+
+// $$ operator 
+
+console.log("And Operator", value4 && value5);
+
+let isLoggedIn = true
+let cardDetails = true
+
+// || operator 
+
+let gmailAccount=true
+let emailAccount=false
+let githubLogin=false
+
+console.log("Login Allowed",gmailAccount || emailAccount || githubLogin);
+
+
+
